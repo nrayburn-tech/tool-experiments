34d3de5f3c8b5e5b6713622ea43716b0c0cd04f0
Nick Rayburn <52075362+nrayburn-tech@users.noreply.github.com>
2024-08-31 19:04:08 -0500

    Reformat all files

--- src/main/kotlin/com/github/nrayburntech/toolexperiments/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nrayburntech/toolexperiments/services/MyProjectService.kt
@@ -1,9 +1,9 @@
 package com.github.nrayburntech.toolexperiments.services
 
+import com.github.nrayburntech.toolexperiments.MyBundle
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import com.github.nrayburntech.toolexperiments.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/main/kotlin/com/github/nrayburntech/toolexperiments/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/nrayburntech/toolexperiments/toolWindow/MyToolWindowFactory.kt
@@ -1,5 +1,7 @@
 package com.github.nrayburntech.toolexperiments.toolWindow
 
+import com.github.nrayburntech.toolexperiments.MyBundle
+import com.github.nrayburntech.toolexperiments.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
@@ -8,8 +10,6 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import com.github.nrayburntech.toolexperiments.MyBundle
-import com.github.nrayburntech.toolexperiments.services.MyProjectService
 import javax.swing.JButton
 
 
